e687cf9aaaa4e457cfac9acddc9dfc189223b53c
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-07-04 18:04:48 +0000

    test

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """Basic annotations -floor"""
+
+
 import math
 
 
-def floor(n: float) -> float:
+def floor(n: float) -> int:
     """returns the floor of the float"""
     return math.floor(n)
