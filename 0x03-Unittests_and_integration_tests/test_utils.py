e115d943aa32db801440a057fe37a8b210c2178e
Zahra11Mosbal11 <zahramosbal@gmail.com>
2024-07-30 16:33:38 +0000

    ty

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -14,6 +14,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        """test access neseted map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
@@ -21,6 +22,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": 1}, ("a", "b"))
     ])
     def test_access_nested_map_exception(self, nested_map, path):
+         """test access neseted map"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
